4308355fb1c48fdca2cce057823783aa008cb97d
Cristian <crisgues8@gmail.com>
2024-01-07 18:56:47 -0400

    notes commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ const App = (props) => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
 
-  const addNote = (event) => {
+  const addNote = event => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
+      important: Math.random() < 0.5,
     }
   
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response)
+      })
   }
 
   const handleNoteChange = (event) => {
